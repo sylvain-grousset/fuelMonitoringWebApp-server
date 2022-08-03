89a9cf9e6002bcc7592c883bc3c3681785c19e96
sylvain-grousset <68558176+sylvain-grousset@users.noreply.github.com>
2022-08-03 13:24:29 +0200

    Update : New route /get2Lasts

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HistoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     //return view('welcome');
 });
 
 Route::get('/getAll', [App\Http\Controllers\HistoController::class, 'getAll']);
+Route::get('/get2Lasts', [App\Http\Controllers\HistoController::class, 'get2Lasts']);
