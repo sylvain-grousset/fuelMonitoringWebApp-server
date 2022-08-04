2a11f52d9a3395746192719ce84d34ff3530fbf9
sylvain-grousset <68558176+sylvain-grousset@users.noreply.github.com>
2022-08-04 11:12:36 +0200

    Update : routes with middleware to Cors

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HistoController;
 
 
+
 Route::get('/', function () {
     //return view('welcome');
 });
 
-Route::get('/getAll', [App\Http\Controllers\HistoController::class, 'getAll']);
-Route::get('/get2Lasts', [App\Http\Controllers\HistoController::class, 'get2Lasts']);
+Route::get('/getAll', [App\Http\Controllers\HistoController::class, 'getAll'])->middleware(['api', 'cors']);
+Route::get('/get2Lasts', [App\Http\Controllers\HistoController::class, 'get2Lasts'])->middleware(['api', 'cors']);
